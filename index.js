36b6d7ff3ffbd442c40edd35b35dfcfc80ad12a1
Jonathan Wohl <jon@jonwohl.com>
2015-09-12 14:56:52 -0400

    added try/catch to require

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-var gpio = require('pi-gpio');
-
+try {
+    var gpio = require('pi-gpio');
+} catch(e) {
+    console.error(e);
+}
 /**
  * Example plugin which reads keystrokes from standard in.
  */
 
 module.exports = function(socket, pubsub) {
     'use strict';
+    if (!gpio) {
+        console.error('\n!!!\nGPIO Not available... is this an RPi?\n!!!\n');
+        return;
+    }
     console.log('loading gpio plugin');
 
 };
